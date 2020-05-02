a08b50f577767012c28083c3968f46922d6e8957
samyakbambole <samyak_study@hotmail.com>
2020-05-02 21:22:17 +0530

    Adding a new file

---
+++ Learning/15.If_Statements_2.rb
@@ -0,0 +1,12 @@
+def max(num, num2, num3) # This method will output the greatest number
+     if num1 >= num2 and num1 >= num3
+          return num1
+     elsif num2 >= num1 and num2 >=num3
+          return num2
+     else
+          return num3
+     end
+end
+
+
+puts max (45, 54, 24)
