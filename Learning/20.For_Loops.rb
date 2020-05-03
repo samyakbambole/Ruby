90a8c34c4b7f0af831972ace255d507ac7a3fbb1
samyakbambole <samyak_study@hotmail.com>
2020-05-03 13:27:38 +0530

    Updates

---
+++ Learning/20.For_Loops.rb
@@ -0,0 +1,17 @@
+friends = ["Ayush", "Surabhi", "Yash", "Mr. Doggy", "Github"]
+
+for friend in friends
+     puts friend
+end
+
+friends.each do |friend|
+     puts friend
+end
+
+for index in 0..10
+     puts index
+end
+
+6.times do|index|
+     puts index
+end 
