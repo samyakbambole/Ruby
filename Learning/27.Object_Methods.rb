e29f3c625eeed7129f9a4d2b6c474362d9c11668
samyakbambole <samyak_study@hotmail.com>
2020-05-04 09:54:51 +0530

    Updates

---
+++ Learning/27.Object_Methods.rb
@@ -0,0 +1,21 @@
+class Student
+     attr_accessor :name, :major, :gpa
+     def initialize(name, major, gpa)
+          @name = name
+          @major = major
+          @gpa = gpa
+     end
+
+     def has_honours
+          if @gpa >= 3.5
+               return true
+          end
+          return false
+     end
+end
+
+student1 = Student.new("Samyak", "Programming", 2.6)
+student2 = Student.new("Ayush", "Gaming", 5.3)
+
+puts student1.has_honours
+puts student2.has_honours 
